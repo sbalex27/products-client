9c88ccaabf9cc0bd50d154c145b4d5d7950df436
Alexander Batres <alex2000.sabe@gmail.com>
2022-06-14 16:47:06 -0600

    Index all categories

--- src/views/categories/CategoriesView.vue
+++ src/views/categories/CategoriesView.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
     <h1>Categories</h1>
+    <div v-if="error">
+      <b-alert show variant="danger"
+        >No se han podido cargar las categorías</b-alert
+      >
+    </div>
+    <div v-else>
+      <b-table :fields="fields" :items="categories"></b-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      fields: ["name", "description"],
+      categories: null,
+      error: null,
+    };
+  },
+  mounted() {
+    fetch("https://localhost:7020/Api/Categories")
+      .then((response) => response.json())
+      .then((body) => (this.categories = body))
+      .catch((error) => (this.error = error));
+  },
+};
 </script>
 
 <style>
-
 </style>
